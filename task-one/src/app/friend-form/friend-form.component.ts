b6b3657234bf4a2bcfca55ce8d4765070738731e
Martekode <BrianDesmedt9000@hotmail.com>
2022-08-04 14:43:19 +0200

    tried to add own onpush

---
+++ task-one/src/app/friend-form/friend-form.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-friend-form',
+  // changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './friend-form.component.html',
+  styleUrls: ['./friend-form.component.css']
+
+})
+export class FriendFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
