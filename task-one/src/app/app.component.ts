2093a73a00b1956b4908e2b2ae36df5095be8109
Martekode <BrianDesmedt9000@hotmail.com>
2022-08-08 09:29:47 +0200

    async fetch of allfriends, obama for now

--- task-one/src/app/app.component.ts
+++ task-one/src/app/app.component.ts
@@ -13,8 +13,11 @@ export class AppComponent {
   friendModel = new Friend(null,null, null,null,null);
   neen = "lele";
   lala  : Friend  | null= null;
+  protected _allFriends:Array<any>;
 
   constructor(private addFriendService: AddFriendService){
+    this._allFriends = new Array<any>();
+    
   }
 
   makeNewFriend(){
@@ -22,5 +25,14 @@ export class AppComponent {
     this.lala = this.friendModel;
     let observable = this.addFriendService.addFriend(this.friendModel);
     observable.subscribe(data => console.log(data), error => console.error(error));
+  }
+
+  public async Obama(url:string) : Promise<any>{
+    let response = await fetch(url , {method : 'GET' , headers : {'content-type': 'application/json'}});
+    this._allFriends.push(response);
+  }
+
+  get allfriends(){
+    return this._allFriends;
   }
 }
