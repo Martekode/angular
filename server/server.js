cb2d36b92f111b239594f3cbc3d7e8143129764a
Martekode <BrianDesmedt9000@hotmail.com>
2022-08-08 10:02:11 +0200

    changed default value, to be comform to my class

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.all("/*", function(req, res, next){
 });
 
 
-let allFriends = [{fName: 'Coach', lName: 'Tim', email: 'tim.broos@becode.org', phone: '0469420666', language: 'Javascript'}];
+let allFriends = [{_firstName: 'Coach', _lastName: 'Tim', _email: 'tim.broos@becode.org', _phoneNumber: '0469420666', _favLang: 'Javascript'}];
 
 // Below you can define how your API handles a get or a post request.
 // Try sending a get request to the root, you should get a "Hello from server" back.
